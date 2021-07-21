d0bbd9d626d1520c7018a6299999245da2b728d4
orbitlens <orbit.lens@gmail.com>
2021-07-21 19:11:00 +0300

    Basic SDK integration test

--- SDK/Centaurus.SDK.Test/ClientTests.cs
+++ SDK/Centaurus.SDK.Test/ClientTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading.Tasks;
 using Centaurus.SDK.Models;
 using NUnit.Framework;
 using stellar_dotnet_sdk;
@@ -8,11 +9,11 @@ namespace Centaurus.SDK
     public class SDKTests
     {
         [Test, Explicit]
-        public async void BasicSdkTest()
+        public async Task BasicSdkTest()
         {
-            var alphaUri = new Uri("https://alpha.test.centaurus.stellar.expert");
-            var constellationInfo = await CentaurusApi.GetConstellationInfo(alphaUri);
-            var client = new CentaurusClient(alphaUri, KeyPair.FromSecretSeed("SD626X4FW2GDP4HRCPNOIBVKQ7AANGCKU3UWXT6B7TZFQC5CFWQBCZ2E"), constellationInfo);
+            var alphaEndpoint = "alpha.test.centaurus.stellar.expert";
+            var constellationInfo = await CentaurusApi.GetConstellationInfo(new Uri("https://"+alphaEndpoint));
+            var client = new CentaurusClient(new Uri("wss://" + alphaEndpoint), KeyPair.FromSecretSeed("SD626X4FW2GDP4HRCPNOIBVKQ7AANGCKU3UWXT6B7TZFQC5CFWQBCZ2E"), constellationInfo);
             await client.Connect();
 
             var info = await client.GetAccountData();
